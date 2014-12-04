e527ef105f8742b1e37677e58c5b69530e674cce
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2014-12-04 15:08:50 +0100

    syserr: Remove unused error packet logging code

--- lib/syserr.h
+++ lib/syserr.h
@@ -17,7 +17,5 @@
 void sys_err_setlogfile(FILE*);
 
 void sys_err(int pri, char *filename, int en, int line, char *fmt, ...);
-void sys_errpack(int pri, char *fn, int ln, int en, struct sockaddr_in *peer,
-		 void *pack, unsigned len, char *fmt, ...);
 
 #endif /* !_SYSERR_H */
