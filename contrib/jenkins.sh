b29ff1da554a2654cc593ea243e29cd7883c92da
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-13 19:02:13 -0400

    jenkins: Add the build script from jenkins here
    This can be used to replicate a build issue more easily.

---
+++ contrib/jenkins.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -ex
+
+mkdir deps || true
+cd deps
+osmo-deps.sh libosmocore
+
+cd libosmocore
+autoreconf --install --force
+./configure --prefix=$PWD/../install
+$MAKE $PARALLEL_MAKE install
+
+cd ../../
+autoreconf --install --force
+PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig:$PKG_CONFIG_PATH ./configure
+PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig:$PKG_CONFIG_PATH $MAKE $PARALLEL_MAKE
+PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig:$PKG_CONFIG_PATH LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE distcheck
