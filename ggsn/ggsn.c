osmocom/osmo-ggsn

commit dd266066c7cc96c797034da352cc9d72177c5780

apn_start(): fix clean-up after errors bringing up APN
When there's an interim error (e.g. in resolving the link-local address
or setting up the tun device), apn_start() simply calls apn_stop()
on the not-yet-fully-started apn_ctx.

This only works if apn_stop() doesn't bail out early in case of
a not-started apn_ctx, so let's remove the related check at the
start of the function.

Change-Id: I2917a6258cb73cc12fd9d81296ff0eaa616890b9

ggsn/ggsn.c
@@ -112,9 +112,6 @@ static void pool_close_all_pdp(struct ippool_t *pool)
 
 int apn_stop(struct apn_ctx *apn, bool force)
 {
-	if (!apn->started)
-		return 0;
-
 	LOGPAPN(LOGL_NOTICE, apn, "%sStopping\n", force ? "FORCED " : "");
 	/* check if pools have any active PDP contexts and bail out */
 	pool_close_all_pdp(apn->v4.pool);

commit 958256f5cfd19610ecec8b1130f726b7a6bb17fc

create_context_ind(): Fix crash on apn not found
Program terminated with signal SIGSEGV, Segmentation fault.
0  create_context_ind (pdp=0xb6b391b0 <pdpa>)
    at /usr/src/debug/osmo-ggsn/1.0.0+gitrAUTOINC+ab5e160937-r0/git/ggsn/ggsn.c:453

453             if (!apn->started)

(gdb) bt
0  create_context_ind (pdp=0xb6b391b0 <pdpa>)
    at /usr/src/debug/osmo-ggsn/1.0.0+gitrAUTOINC+ab5e160937-r0/git/ggsn/ggsn.c:453
1  0xb6b225e0 in gtp_create_pdp_ind (gsn=gsn@entry=0x74f28, version=version@entry=1, peer=0x0,
    peer@entry=0xbee6ead4, fd=-1092167056, fd@entry=8, pack=pack@entry=0xbee6eae4, len=len@entry=179)
    at /usr/src/debug/osmo-ggsn/1.0.0+gitrAUTOINC+ab5e160937-r0/git/gtp/gtp.c:1591
2  0xb6b245e4 in gtp_decaps1c (gsn=0x74f28)
    at /usr/src/debug/osmo-ggsn/1.0.0+gitrAUTOINC+ab5e160937-r0/git/gtp/gtp.c:2986
3  0x41d770c0 in osmo_select_main () from /usr/lib/libosmocore.so.8
4  0x000121b8 in main (argc=4, argv=0xbee70e54)
    at /usr/src/debug/osmo-ggsn/1.0.0+gitrAUTOINC+ab5e160937-r0/git/ggsn/ggsn.c:897

Fixes: dd266066c7cc96c797034da352cc9d72177c5780, b16c46b4c36383368eb1581e40d0a8ca751ddc53

Change-Id: Ie4ec74e87aaf1d067dd1717d986673be56c4d6ed

ggsn/ggsn.c
@@ -449,14 +449,14 @@ int create_context_ind(struct pdp_t *pdp)
 	/* First find an exact APN name match */
 	apn = ggsn_find_apn(ggsn, name_buf);
 	/* ignore if the APN has not been started */
-	if (!apn->started)
+	if (apn && !apn->started)
 		apn = NULL;
 
 	/* then try default (if any) */
 	if (!apn)
 		apn = ggsn->cfg.default_apn;
 	/* ignore if the APN has not been started */
-	if (!apn->started)
+	if (apn && !apn->started)
 		apn = NULL;
 
 	if (!apn) {