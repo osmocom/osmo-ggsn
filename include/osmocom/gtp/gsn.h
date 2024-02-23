bad5eeba0fb4c9e203f57bf253a371cb7c0c6345
Oliver Smith <osmith@sysmocom.de>
2024-02-23 12:48:39 +0100

    gtp/gsn_internal.h: new file
    Change-Id: I999462e39411fc4ec7e50bd0212e870006fbc4f1

--- include/osmocom/gtp/gsn.h
+++ include/osmocom/gtp/gsn.h
@@ -175,7 +175,4 @@ extern int gtp_fd(struct gsn_t *gsn);
 extern int gtp_retrans(struct gsn_t *gsn) OSMO_DEPRECATED("This API is a no-op, libgtp already does the job internally");
 extern int gtp_retranstimeout(struct gsn_t *gsn, struct timeval *timeout) OSMO_DEPRECATED("This API is a no-op and will return a 1 day timeout");
 
-/* Internal APIs: */
-void gtp_queue_timer_start(struct gsn_t *gsn);
-
 #endif /* !_GSN_H */
